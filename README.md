d0de7648d6c29a9823cf91539c23e3b276043191
jose.fragozo <ing.joselfragozo@gmail.com>
2020-02-10 22:49:56 -0500

    experimento V1

--- README.md
+++ README.md
@@ -10,3 +10,12 @@ Vistas de Arquitectura
 * [Diagrama de componentes](https://github.com/arti4109-arquitectura-de-software/g5-reto1-2020-01-mati-g5/wiki/Diagrama-de-Componentes%E2%80%8B)
 * [Diagrama de concurrencia](https://github.com/arti4109-arquitectura-de-software/g5-reto1-2020-01-mati-g5/wiki/Diagrama-de-concurrencia)
 * [Diagrama de despliegue](https://github.com/arti4109-arquitectura-de-software/g5-reto1-2020-01-mati-g5/wiki/Diagrama-de-despliegue%E2%80%8B)
+
+
+Descripcion Experimento
+- se queria determinar la capacidad de un nodo para notificar eventos a un determinado cliente
+- el atributo de calidad era el rendimiento de un nodo.
+
+Hipotesis 
+- poder despachar 500K de eventos en menos de 30 segundos
+
---
+++ SignalRTest/hubs/TestHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRTest.hubs
+{
+    public class TestHub : Hub
+    {        
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task NotifyAll() 
+        {
+            var sw = Stopwatch.StartNew();
+            for (int i = 0; i < 500000; i++)
+            {
+                await Clients.All.SendAsync("ReceiveMessage", "trading engine", $"oferta ha cambiado {i}");
+            }
+            sw.Stop();
+            var total = Convert.ToDecimal(sw.ElapsedMilliseconds) / 1000m;
+            await Clients.All.SendAsync("ReceiveMessage", "trading engine", "rafaga terminada en: " + total + " segs ");
+        }
+    }
+}
